c3bb02071ae2b8138493dc2d7bced1ec2c14204c
max <109116116+xlcz@users.noreply.github.com>
2023-06-19 22:44:56 +0100

    No Collision Initial Push

---
+++ no_collision/NoCollision/NoCollision/Plugin.cs
@@ -0,0 +1,23 @@
+﻿using BepInEx;
+using HarmonyLib;
+using SyncMultiplayer;
+
+namespace NoCollision
+{
+    [BepInPlugin("nc-max", "No Collision", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            Logger.LogInfo($"Plugin No Collision is loaded!");
+            Harmony.CreateAndPatchAll(typeof(Plugin));
+        }
+
+        [HarmonyPatch(typeof(NetGameSubroomsSystem), "SEND_ChangeRoomID")]
+        [HarmonyPostfix]
+        private static void SEND_ChangeRoomID(NetworkPlayer __0, string __1)
+        {
+            GameConsole.SetCollisions(false);
+        }
+    }
+}
