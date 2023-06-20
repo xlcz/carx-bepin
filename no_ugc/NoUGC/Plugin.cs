875940982d605f934a358665c86f742924c5cab7
max <109116116+xlcz@users.noreply.github.com>
2023-06-20 20:34:13 +0100

    No UGC push

---
+++ no_ugc/NoUGC/Plugin.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using BepInEx;
+using Customization;
+using GameOverlay;
+using HarmonyLib;
+using Sfs2X.Entities;
+using Steamworks.Ugc;
+using SyncMultiplayer;
+using UnityEngine;
+
+namespace NoUGC
+{
+    [BepInPlugin("nugc-max", "No UGC", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            Logger.LogInfo($"Plugin No UGC is loaded!");
+            Harmony.CreateAndPatchAll(typeof(Plugin));
+        }
+
+        [HarmonyPatch(typeof(SteamUGCManager), "Update")]
+        [HarmonyPrefix]
+        private static bool Update(SteamUGCManager __instance)
+        {
+            Traverse.Create(__instance).Method("PopPendingPackage", new Type[0]); // Pop the package from ugcPackagesQueue
+            return false; // Prevent UGC file from being loaded / downloaded
+        }
+    }
+}
